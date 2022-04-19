b9309d800e403517f0cc2234b1ae4c30fabbc050
Murilo <murilocarrijoadm@gmail.com>
2022-04-19 17:13:28 -0300

    validacao por token para realizar o getAll - requisito 03

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
 const add = async (req, res, _next) => {
@@ -24,10 +25,17 @@ const add = async (req, res, _next) => {
   }
 };
 
-const getAll = async (_req, res, _next) => {
+const getAll = async (req, res, _next) => {
+  const { authorization } = req.headers;
   try {
+    try {
+      if (authorization.length === 0) return res.status(401).json({ message: 'Token not found' });
+      jwt.verify(authorization, 'superSecreto');
+    } catch (e) {
+      console.log(e);
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
     const users = await User.findAll();
-
     return res.status(200).json(users);
   } catch (e) {
     console.log(e);
